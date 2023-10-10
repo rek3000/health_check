fb86a9d9abbcaf72dc43a339ec5691a48052a3fa
rek3000 <torek3k@gmail.com>
2023-10-10 17:27:07 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def grep(path, word):
+    with open(path, 'r') as file:
+        content = file.readlines()
+
+        count = 0
+        for line in range(0, len(content)):
+            if content[line].find(word) != -1:
+                print(line + 1, ': ', content[line], sep='', end='')
+            
+def print_file(path):
+    with open(path, 'r') as file:
+        content = file.readlines()
+        count = 0
+        for line in range(0, len(content)):
+            print(line + 1, ': ', content[line], sep='', end='')
+
+def main():
+
+    path=input('Enter path to log folder(ILOM): ')
+    print_file(path + '/fma/@usr@local@bin@fmadm_faulty.out')
+    print()
+    grep(path + '/ilom/@usr@local@bin@collect_properties.out', '_temp')
+    print()
+    grep(path + '/ilom/@usr@local@bin@spshexec_version.out', 'SP firmware')
+
+
+if __name__ == "__main__":
+    main()
