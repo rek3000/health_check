1c0b756bfeb2285d31519caa12d568e97ff64485
rek3000 <torek3k@gmail.com>
2023-10-12 15:10:48 +0700

    12/10/2023: refactoring

--- main.py
+++ main.py
@@ -2,31 +2,19 @@
 import os
 import shutil
 import glob
+import re
 import json
 import zipfile
 import tarfile
+import sys
 
-checklist = [
-        [1, "Kiểm tra nhiệt độ", ""],
-        [2, "Kiểm tra phiên bản ILOM firmware", ""],
-        [3, "Kiểm tra trạng thái phần cứng", ""],
-        [4, "Kiểm tra phiên bản Image", ""],
-        [5, "Kiểm tra cấu hình RAID và dung lượng phân vùng OS", ""],
-        [6, "Kiểm tra cấu hình network", ""],
-        [7, "Kiểm tra CPU Utilization", ""],
-        [8, "Kiểm tra Flash IO", ""],
-        [9, "Kiểm tra Flash Bandwidth", ""],
-        [10, "Kiểm tra Hard Disk IO", ""],
-        [11, "Kiểm tra Hard Disk Bandwidth", ""],
-    ]
-
-#### PATH 
-## ORACLE SOLARIS
-FAULT='/fma/@usr@local@bin@fmadm_faulty.out'
-TEMP='/ilom/@usr@local@bin@collect_properties.out'
-FIRMWARE='/ilom/@usr@local@bin@collect_properties.out'
-IMAGE='/patch+pkg/pkg_list-af_entire.out' # Solaris
-PARTITION='/disks/df-kl.out'
+##### DEFAULT PATHS #####
+## ORACLE LINUX
+FAULT_LINUX='/fma/@usr@local@bin@fmadm_faulty.out'
+TEMP_LINUX='/ilom/@usr@local@bin@collect_properties.out'
+FIRMWARE_LINUX='/ilom/@usr@local@bin@collect_properties.out'
+IMAGE_LINUX='' # Solaris
+PARTITION_LINUX='/disks/df-kl.out'
 RAID='' 
 NETWORK='/sysconfig/ifconfig-a.out'
 CPU_ULTILIZATION=''
@@ -34,35 +22,45 @@ CPU_LOAD=''
 MEMORY=''
 SWAP=''
 EXTRACT_LOCATION='./temp/'
-#TODO ## ORACLE LINUX
-# FAULT='/fma/@usr@local@bin@fmadm_faulty.out'
-# TEMP='/ilom/@usr@local@bin@collect_properties.out'
-# FIRMWARE='/ilom/@usr@local@bin@collect_properties.out'
-# IMAGE='/patch+pkg/pkg_list-af_entire.out' # Solaris
-# PARTITION='/disks/df-kl.out'
-# RAID='' 
-# NETWORK='sysconfig/ifconfig-a.out'
-# CPU_ULTILIZATION=''
-# CPU_LOAD=''
-# MEMORY=''
-# SWAP=''
-####
+##
+## ORACLE SOLARIS
+FAULT_SOL='/fma/@usr@local@bin@fmadm_faulty.out'
+TEMP_SOL='/ilom/@usr@local@bin@collect_properties.out'
+FIRMWARE_SOL='/ilom/@usr@local@bin@collect_properties.out'
+IMAGE_SOL='/etc/release'
+PARTITION='/disks/df-kl.out'
+RAID='' 
+NETWORK='/sysconfig/ifconfig-a.out'
+CPU_ULTILIZATION=''
+CPU_LOAD=''
+MEMORY=''
+SWAP=''
+##
+##### END_PATHS #####
 
 ##### IMPLEMETATION #####
-def clean_up():
+def clean_files():
     folder = './temp/'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+            os.unlink(./output.txt)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def clean_up():
     print('Remove unzip files? (y/n) ', end='')
     choice = input('')
     if choice in ['yes', 'y', 'Y', 'yeah', 'YES']:
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        clean_files()
+
+def clean_up_force():
+    print('FORCE CLEAN UP DUE TO ERROR!')
+    clean_files()
 
 def check_valid(path):
     return os.path.isdir(path)
@@ -71,53 +69,47 @@ def extract_file(serial, compress):
     compress = compress.lower()
     file = get_file(serial, compress) 
     if file == -1: return -1
-    print(file)
-
-    #This sucks, fix later
+    print('Extracting: ', file)
     if compress == 'zip':
-        if zipfile.is_zipfile(file) is False:
-            return -1
-        with zipfile.ZipFile(file, 'r') as z_object:
-            z_object.extractall(path='./temp/')
-        z_object.close()
-
-        # return the file name without extension
-        return file.split('/')[1][:-len(compress)-1]
-
+        unzip(file)
+        return rm_ext(file, compress)
     elif compress == 'tar.gz':
-        if tarfile.is_tarfile(file) is False:
-            return -1
-        with tarfile.open(file, 'r:*') as t_object:
-            t_object.extractall(path='./temp/', numeric_owner=True)
-        t_object.close()
+        untar(file)
+        return rm_ext(file, compress)
+    else: return -1
 
-        # return the file name without extension
-        return file.split('/')[1][:-len(compress)-1] 
-    else: 
-        return -1
-
+# Find return the file with serial number 
 def get_file(serial, compress):
-    # This sucks too, works for now
-    regex = "sample/*" + serial + "*." + compress
-    files = glob.glob(regex)
+    #works for now
+    root = './sample/'
+    regex = '*[_.]' + serial + '[_.]*.' + compress
+
+    files = glob.glob(os.path.join(root, regex))
+    print(files)
     if len(files) == 1:
         return files[0]
     elif len(files) > 1:
         i = 0
         for file in files:
-            print(i,':', file)
+            print('[',i,'] ', file, sep='')
             i += 1
-        choice = int(input('Which file? [n] '))
+        choice = int(input('Which file? [i] '))
         return files[choice]
-    else:
-        return -1
+    else: return -1
 
 def get_content(path):
-    fault = print_file(path + FAULT)
-    temp = grep(path + TEMP, '_temp')
-    firmware = grep(path + FIRMWARE, 'SP firmware')
-    image = print_file(path + IMAGE)
-    partition = print_file(path + PARTITION) 
+    # SUCKS
+    root = './temp/'
+    for i in range(0, len(path)):
+        path[i] = root + path[i]
+
+    # path[0] = ILOM | path[1] = EXPLORER
+    fault = print_file(path[0] + FAULT_SOL)
+    temp = grep(path[0] + TEMP_SOL, '_temp')
+    firmware = grep(path[0] + FIRMWARE_SOL, 'Version')
+    # image = print_file(path[1] + IMAGE_SOL)
+    image = grep(path[1] + IMAGE_SOL, 'Solaris')
+    partition = print_file(path[1] + PARTITION_SOL) 
     content = fault+temp+firmware+image+partition
     return content
 
@@ -146,28 +138,53 @@ def print_file(path):
     print()
     return result + '\n'
 
+def unzip(file):
+    if not zipfile.is_zipfile(file):
+            return -1
+    with zipfile.ZipFile(file, 'r') as z_object:
+        z_object.extractall(path='./temp/')
+
+def untar(file):
+    if not tarfile.is_tarfile(file):
+        return -1
+    with tarfile.open(file, 'r:*') as t_object:
+        # t_object.extractall(path='./temp/', numeric_owner=True)
+        t_object.extractall(path='./temp/')
+
 def save_file(file, content):
     with open(file, 'w') as file:
         file.write(content)
 
+def rm_ext(file, compress):
+    return file.split('/')[2][:-len(compress)-1]
+
 def run():
-    # serial = input('Enter serial number: ')
-    # path = extract_file(serial, 'zip')
-    serial2 = input('Enter serial number (2): ')
-    path = extract_file(serial2, 'tar.gz') ### Quick test 
+    serial = input('Enter serial numbers [ilom & explorer]: ')
+    serial = serial.split(' ')
+    if len(serial) != 2: return -1
+
+    path = ['','']
+    path[0] = extract_file(serial[0], 'zip')
+    path[1] = extract_file(serial[1], 'tar.gz') ### Quick test 
+    if path == [-1, -1]: return -1
+
+    # if sys.argv[0] == '-z':
+    #     extract_file('*', 'tar.gz'
+
     print('PATH: ', path)
     if path == -1: 
+        clear_up_force()
         return -1 
 
-    # content = get_content('./temp/' + path)
-    # save_file('output.txt', content)
+    content = get_content(path)
+    save_file('output.txt', content)
+##### END_IMPLEMENTATION #####
 
 ##### MAIN #####
 def main():
-    if run() == -1:
-        clean_up()
-        return -1
+    if run() == -1: return -1
     clean_up()
 
 if __name__ == "__main__":
     main()
+##### END_MAIN #####
