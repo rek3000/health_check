4c3a346ab56524faa19472bd4ff0b6c3381b8fa3
rek3000 <torek3k@gmail.com>
2023-10-19 15:53:37 +0700

    enhance debugging message

--- rek.py
+++ rek.py
@@ -23,9 +23,9 @@ EXTRACT_LOCATION='./temp/'
 #HugePages = HugePages_Total * 2 /1024 = ~ 67.8% physical Memory
 ##
 ## ORACLE SOLARIS
-FAULT_SOL='/fma/@usr@local@bin@fmadm_faulty.out'
-TEMP_SOL='/ilom/@usr@local@bin@collect_properties.out'
-FIRMWARE_SOL='/ilom/@usr@local@bin@collect_properties.out'
+FAULT='/fma/@usr@local@bin@fmadm_faulty.out'
+TEMP='/ilom/@usr@local@bin@collect_properties.out'
+FIRMWARE='/ilom/@usr@local@bin@collect_properties.out'
 IMAGE_SOL='/etc/release'
 PARTITION_SOL='/disks/df-kl.out'
 RAID_SOL='/disks/zfs/zpool_status_-v.out' 
@@ -50,7 +50,7 @@ def clean_files():
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
-            sys.exit()
+            return -1
 
 def clean_up():
     print('Remove unzip files? (y/n) ', end='')
@@ -87,7 +87,7 @@ def get_file(serial, compress):
     files = glob.glob(os.path.join(root, regex))
     if len(files) == 0:
         print('No file found matched with the serial list!')
-        sys.exit()
+        return -1
     elif len(files) == 1:
         return files[0]
     else:
@@ -100,7 +100,7 @@ def get_file(serial, compress):
 def save_json(file, content):
     if not content:
         print('No content from input to save!')
-        sys.exit()
+        return -1
 
     try:
         with open(file, 'w') as file:
@@ -108,7 +108,7 @@ def save_json(file, content):
     except OSError as err:
         print('OS error: ', err)
         raise RuntimeError('Cannot save JSON') from err
-        sys.exit()
+        return -1
 
 def read_json(file):
     try:
@@ -118,7 +118,7 @@ def read_json(file):
     except OSError as err:
         print('OS error: ', err)
         raise RuntimeError('Cannot read JSON') from err
-        sys.exit()
+        return -1
 
 def join_json(output):
     try:
@@ -132,7 +132,7 @@ def join_json(output):
             json.dump(z, file, indent=4)
     except OSError as err:
         print('OS error: ', err)
-        sys.exit()
+        return -1
 
 # NO MORE SUBPROCESS MORON
 def grep(path, regex, uniq=True):
@@ -156,8 +156,79 @@ def grep(path, regex, uniq=True):
     # print(result)
     print()
     return result
+
 def get_ilom(path):
     print('filler')
+    fault = cat(path + FAULT).strip()
+
+    inlet_temp = grep(path + TEMP, 'inlet_temp').strip().split()
+    inlet_temp = ' '.join(inlet_temp[2:5])
+
+    exhaust_temp = grep(path + TEMP, 'exhaust_temp').strip().split()
+    exhaust_temp = ' '.join(exhaust_temp[2:5])
+
+    firmware = grep(path + FIRMWARE, 'Version').strip().split()
+    firmware = ' '.join(firmware[1:])
+
+    return {'fault': fault, 'inlet': inlet_temp, 'exhaust': exhaust_temp, 'firmware': firmware} 
+
+def get_os(path, type='SOL'):
+    x = {}
+    if type == 'SOL':
+        image = grep(path + IMAGE_SOL, 'Solaris').strip().split()
+        image = image[2]
+        x['image'] = image
+
+        vol = grep(path + PARTITION_SOL, "\\B\/\\B").strip().split()
+        vol = vol[-2]
+        vol_avail = str(100 - int(vol[:-1])) + '%'
+        x['vol_avail'] = vol_avail
+
+        raid = grep(path + RAID_SOL, "mirror").strip().split()
+        if 'ONLINE' in raid:
+            raid_stat = True 
+        else:
+            raid_stat = False
+        x['raid_stat'] = raid_stat
+
+        net_ipmp = grep(path + NETWORK_SOL, 'ipmp')
+        net_aggr = grep(path + NETWORK_SOL, 'aggr')
+        if not net_ipmp and not net_aggr:
+            net = 'none'
+        elif net_ipmp and not net_aggr:
+            net = 'ipmp'
+        elif net_aggr and not net_ipmp:
+            net = 'aggr'
+        else:
+            net = 'both'
+        x['net'] = net
+
+        cpu_util = cat(path + CPU_ULTILIZATION_SOL).strip().split('\n')
+        cpu_util = cpu_util[2]
+        cpu_util = cpu_util.split()[21]
+        cpu_util = str(100 - int(cpu_util)) + '%'
+        x['cpu_util'] = cpu_util
+
+        load = grep(path + CPU_LOAD_SOL, 'load average').strip().split(', ')
+        load = ' '.join(load).split()[-3:]
+        load_avg = str(max(load))
+        x['load_avg'] = load_avg
+
+        mem = grep(path + MEM_SOL, 'freelist', False).strip().split()
+        mem_free = mem[-1]
+        mem_util = str(100 - int(mem_free[:-1])) + '%'
+        x['mem_util'] = mem_util
+
+        swap = cat(path + SWAP_SOL).strip().split()
+        swap = [swap[8], swap[10]]
+        swap[0] = int(swap[0][:-2])
+        swap[1] = int(swap[1][:-2])
+        swap_util = swap[0] / (swap[0] + swap[1])
+        swap_util = str(int(swap_util * 100)) + '%'
+        x['swap_util'] = swap_util
+
+        print(x)
+    return x
 
 def get_content(path):
     root = './temp/'
@@ -166,76 +237,25 @@ def get_content(path):
     for i in range(0, len(path)):
         path[i] = root + str(path[i])
 
-    # @@ SUCKS 
-    fault = cat(path[0] + FAULT_SOL).strip()
-
-    inlet_temp = grep(path[0] + TEMP_SOL, 'inlet_temp').strip().split()
-    inlet_temp = ' '.join(inlet_temp[2:5])
-
-    exhaust_temp = grep(path[0] + TEMP_SOL, 'exhaust_temp').strip().split()
-    exhaust_temp = ' '.join(exhaust_temp[2:5])
-
-    firmware = grep(path[0] + FIRMWARE_SOL, 'Version').strip().split()
-    firmware = ' '.join(firmware[1:])
-
-    image = grep(path[1] + IMAGE_SOL, 'Solaris').strip().split()
-    image = image[2]
-
-    vol = grep(path[1] + PARTITION_SOL, "\\B\/\\B").strip().split()
-    vol = vol[-2]
-    vol_avail = str(100 - int(vol[:-1])) + '%'
-    raid = grep(path[1] + RAID_SOL, "mirror").strip().split()
-    if 'ONLINE' in raid:
-        raid_stat = True 
-    else:
-        raid_stat = False
-
-    net_ipmp = grep(path[1] + NETWORK_SOL, 'ipmp')
-    net_aggr = grep(path[1] + NETWORK_SOL, 'aggr')
-    if  not net_ipmp and not net_aggr:
-        net = 'none'
-    elif net_ipmp and not net_aggr:
-        net = 'ipmp'
-    elif net_aggr and not net_ipmp:
-        net = 'aggr'
-    else:
-        net = 'both'
-
-    cpu_util = cat(path[1] + CPU_ULTILIZATION_SOL).strip().split('\n')
-    cpu_util = cpu_util[2]
-    cpu_util = cpu_util.split()[21]
-    cpu_util = str(100 - int(cpu_util)) + '%'
-
-    load = grep(path[1] + CPU_LOAD_SOL, 'load average').strip().split(', ')
-    load = ' '.join(load).split()[-3:]
-    load_avg = str(max(load))
-
-    mem = grep(path[1] + MEM_SOL, 'freelist', False).strip().split()
-    mem_free = mem[-1]
-    mem_util = str(100 - int(mem_free[:-1])) + '%'
-
-    swap = cat(path[1] + SWAP_SOL).strip().split()
-    swap = [swap[8], swap[10]]
-    swap[0] = int(swap[0][:-2])
-    swap[1] = int(swap[1][:-2])
-    swap_util = swap[0] / (swap[0] + swap[1])
-    swap_util = str(int(swap_util * 100)) + '%'
-
+    # @@
+    ilom = get_ilom(path[0])
+    os = get_os(path[1], 'SOL')
     name = org_path.split('_')[0]
+
     content = {}
     content[name] = {
-            'fault': fault,
-            'inlet': inlet_temp,
-            'exhaust': exhaust_temp,
-            'firmware': firmware,
-            'image': image,
-            'vol_avail': vol_avail,
-            'raid_stat': raid_stat,
-            'net': net,
-            'cpu_util': cpu_util,
-            'load_avg': load_avg,
-            'mem_free': mem_free,
-            'swap_util': swap_util,
+            'fault': ilom['fault'],
+            'inlet': ilom['inlet'],
+            'exhaust': ilom['exhaust'],
+            'firmware': ilom['firmware'],
+            'image': os['image'],
+            'vol_avail': os['vol_avail'],
+            'raid_stat': os['raid_stat'],
+            'net': os['net'],
+            'cpu_util': os['cpu_util'],
+            'load_avg': os['load_avg'],
+            'mem_util': os['mem_util'],
+            'swap_util': os['swap_util'],
     }
     return content
 
@@ -254,30 +274,38 @@ def cat(path, stdout=True):
                 for l in content:
                     result += l.lstrip()
                 return io.StringIO(result)
-    except:
-        print('Error: Cannot read file')
+    except Exception as err:
+        raise RuntimeError('Cannot open file to read') from err
         return -1
 
 def unzip(file):
     if not zipfile.is_zipfile(file):
         print('Error: Not a zip file')
         return -1
-    with zipfile.ZipFile(file, 'r') as z_object:
-        z_object.extractall(path='./temp/')
+    try:
+        with zipfile.ZipFile(file, 'r') as z_object:
+            z_object.extractall(path='./temp/')
+    except Exception as err:
+        print('Error:' , err)
+        return -1
 
 def untar(file):
     if not tarfile.is_tarfile(file):
         print('Error: Not a tar file')
         return -1
-    with tarfile.open(file, 'r:*') as t_object:
-        t_object.extractall(path='./temp/', numeric_owner=True)
+    try:
+        with tarfile.open(file, 'r:*') as t_object:
+            t_object.extractall(path='./temp/', numeric_owner=True)
+    except Exception as err:
+        print('Error:' , err)
+        return -1
 
 def save_file(file, content):
     try:
         with open(file, 'w') as file:
             file.write(content)
-    except:
-        print('Error: Cannot write file')
+    except OSError as err:
+        raise RuntimeError('Cannot save file: ') from err
         return -1
 
 def rm_ext(file, compress):
@@ -287,8 +315,8 @@ def extract_info():
     try:
         with open('./input', 'r') as file:
             number = file.readlines()
-    except:
-        print('Serial input file not found')
+    except Exception as err:
+        print(err)
         return -1
 
     output_files = []
@@ -300,13 +328,16 @@ def extract_info():
     #     serial = input('Enter serial numbers [ilom & explorer]: ')
         serial = serial.split(' ')
         if len(serial) != 2: 
+            print('Error: Not enough serial!')
             return -1
 
         path = ['','']
         path[0] = extract_file(serial[0], 'zip')
         path[1] = extract_file(serial[1], 'tar.gz')
 
-        if path == [-1, -1]: return -1
+        if path == [-1, -1]: 
+            print('Error: PATH not exist!')
+            return -1
 
         print('PATH: ', path)
 
@@ -315,13 +346,15 @@ def extract_info():
 
         data = './output/' + data + '.json'
         content = get_content(path)
-        save_json(data, content)
+        if save_json(data, content) == -1:
+            return -1 
     return output_files
 
 # MAIN 
 def run():
     output_files = extract_info()
     if output_files == -1:
+        print('Error: No files to join!')
         return -1
     choice = input('Join all input?[y/n] ')
     if choice in ['', 'yes', 'y', 'Y', 'yeah', 'YES']:
@@ -330,7 +363,9 @@ def run():
 
 ##### MAIN #####
 def main():
-    if run() == -1: return -1
+    if run() == -1: 
+        clean_up_force()
+        return -1
     clean_up()
 
 if __name__ == "__main__":
