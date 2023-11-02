7bb8c9ad9de653343138b0d285496af8a36b4698
rek3000 <torek3k@gmail.com>
2023-11-02 16:43:34 +0700

    ?

--- rekdoc/doc.py
+++ rekdoc/doc.py
@@ -27,92 +27,56 @@ def assert_data(data):
             continue
         if i == "mem_util":
             i = "mem_free"
-        asserted[i] = ["", ""]
+        asserted[i] = ["", []]
     if data["fault"] == "No faults found":
         asserted["fault"][0] = 5
-        asserted["fault"][1] = "Lỗi: Không\nĐánh giá: " + ASSERTION[5]
+        asserted["fault"][1].extend(["Lỗi: Không", "Đánh giá: " + ASSERTION[5]])
     else:
         asserted["fault"][0] = 1
-        asserted["fault"][1] = (
-            "Lỗi ảnh hưởng tới hoạt động của hệ thống\nĐánh giá: " + ASSERTION[1]
-        )
+        asserted["fault"][1].extend(["Lỗi ảnh hưởng tới hoạt động của hệ thống", "Đánh giá: " + ASSERTION[1]])
 
     temp = data["inlet"].split()[0]
     temp = int(temp)
     if temp >= 21 and temp <= 23:
         asserted["temp"][0] = 5
-        asserted["temp"][1] = (
-            "Nhiệt độ bên trong: " + str(temp) + "\nĐánh giá: " + ASSERTION[5]
-        )
+        asserted["temp"][1].extend(["Nhiệt độ bên trong: " + str(temp), "Đánh giá: " + ASSERTION[5]])
     elif temp > 23 and temp <= 26:
         asserted["temp"][0] = 3
-        asserted["temp"][1] = (
-            "Nhiệt độ bên trong: " + str(temp) + "\nĐánh giá: " + ASSERTION[3]
-        )
+        asserted["temp"][1].extend(["Nhiệt độ bên trong: " + str(temp), "Đánh giá: " + ASSERTION[5]])
     elif temp > 26:
         asserted["temp"][0] = 1
-        asserted["temp"][1] = (
-            "Nhiệt độ bên trong: " + str(temp) + "\nĐánh giá: " + ASSERTION[1]
-        )
+        asserted["temp"][1].extend(["Nhiệt độ bên trong: " + str(temp), "Đánh giá: " + ASSERTION[5]])
 
     asserted["firmware"][0] = data["firmware"]
-    asserted["firmware"][1] = (
-        "Phiên bản Ilom hiện tại: " + data["firmware"] + "\nPhiên bản Ilom mới nhất: "
-    )
+    asserted["firmware"][1].extend(["Phiên bản Ilom hiện tại: " + data["firmware"], "Phiên bản Ilom mới nhất: "])
 
     asserted["image"][0] = data["image"]
-    asserted["image"][1] = (
-        "Phiên bản OS hiện tại: " + data["image"] + "\nPhiên bản OS mới nhất:"
-    )
+    asserted["image"][1].extend(["Phiên bản OS hiện tại: " + data["image"], "Phiên bản OS mới nhất: "])
 
     if data["vol_avail"] > 30 and data["raid_stat"] == True:
         asserted["vol_avail"][0] = 5
-        asserted["vol_avail"][1] = (
-            "Phân vùng OS được cấu hình RAID \nDung lượng khả dụng: "
-            + str(data["vol_avail"])
-            + "%"
-            + "\nĐánh giá: "
-            + ASSERTION[5]
-        )
+        asserted["vol_avail"][1].extend( ["Phân vùng OS được cấu hình RAID", "Dung lượng khả dụng: " + str(data["vol_avail"]) + "%", "Đánh giá: " + ASSERTION[5]] )
     elif (data["vol_avail"] > 15 and data["vol_avail"] <= 30) and data[
         "raid_stat"
     ] == True:
         asserted["vol_avail"][0] = 3
-        asserted["vol_avail"][1] = (
-            "Phân vùng OS được cấu hình RAID \nDung lượng khả dụng: "
-            + str(data["vol_avail"])
-            + "%"
-            + "\nĐánh giá: "
-            + ASSERTION[3]
-        )
+        asserted["vol_avail"][1].extend( ["Phân vùng OS được cấu hình RAID", "Dung lượng khả dụng: " + str(data["vol_avail"]) + "%", "Đánh giá: " + ASSERTION[3]] )
     elif data["vol_avail"] <= 15 and data["raid_stat"] == False:
         asserted["vol_avail"][0] = 1
-        asserted["vol_avail"][1] = (
-            "Phân vùng OS không được cấu hình RAID \nDung lượng khả dụng: "
-            + str(data["vol_avail"])
-            + "%"
-            + "\nĐánh giá: "
-            + ASSERTION[1]
-        )
+        asserted["vol_avail"][1].extend( ["Phân vùng OS không được cấu hình RAID", "Dung lượng khả dụng: " + str(data["vol_avail"]) + "%", "Đánh giá: " + ASSERTION[1]] )
     elif data["vol_avail"] <= 15 and data["raid_stat"] == True:
         asserted["vol_avail"][0] = 1
-        asserted["vol_avail"][1] = (
-            "Phân vùng OS được cấu hình RAID \nDung lượng khả dụng: "
-            + str(data["vol_avail"])
-            + "%"
-            + "\nĐánh giá: "
-            + ASSERTION[1]
-        )
+        asserted["vol_avail"][1].extend( ["Phân vùng OS được cấu hình RAID", "Dung lượng khả dụng: " + str(data["vol_avail"]) + "%", "Đánh giá: " + ASSERTION[1]] )
 
     if data["bonding"] == "none":
         asserted["bonding"][0] = 1
-        asserted["bonding"][1] = "Network không được cấu hình bonding"
+        asserted["bonding"][1].extend( ["Network không được cấu hình bonding"])
     elif data["bonding"] == "aggr":
         asserted["bonding"][0] = 5
-        asserted["bonding"][1] = "Network được cấu hình bonding Aggregration"
+        asserted["bonding"][1].extend( ["Network được cấu hình bonding Aggregration"])
     elif data["bonding"] == "ipmp":
         asserted["bonding"][0] = 5
-        asserted["bonding"][1] = "Network được cấu hình bonding IPMP"
+        asserted["bonding"][1].extend( ["Network được cấu hình bonding IPMP"])
 
     if data["cpu_util"] <= 30:
         asserted["cpu_util"][0] = 5
@@ -120,7 +84,7 @@ def assert_data(data):
         asserted["cpu_util"][0] = 3
     else:
         asserted["cpu_util"][0] = 1
-    asserted["cpu_util"][1] = "CPU Ultilization khoảng " + str(data["cpu_util"]) + "%"
+    asserted["cpu_util"][1].extend( ["CPU Ultilization khoảng " + str(data["cpu_util"]) + "%"])
 
     if data["load"]["load_avg_per"] <= 2:
         asserted["load"][0] = 5
@@ -128,14 +92,7 @@ def assert_data(data):
         asserted["load"][0] = 3
     else:
         asserted["load"][0] = 1
-    asserted["load"][1] = (
-        "CPU Load Average: "
-        + str(data["load"]["load_avg"])
-        + "\nNumber of Cores: "
-        + str(data["load"]["vcpu"])
-        + "\nCPU Load Average per core = CPU Load Average / Number of Cores = "
-        + str(data["load"]["load_avg_per"])
-    )
+    asserted["load"][1].extend( ["CPU Load Average: " + str(data["load"]["load_avg"]), "Number of Cores: " + str(data["load"]["vcpu"]), "CPU Load Average per core = CPU Load Average / Number of Cores = " + str(data["load"]["load_avg_per"])] )
 
     mem_free = 100 - data["mem_util"]
     if mem_free >= 20:
@@ -144,7 +101,7 @@ def assert_data(data):
         asserted["mem_free"][0] = 3
     else:
         asserted["mem_free"][0] = 1
-    asserted["mem_free"][1] = "Physical memory free: " + str(mem_free) + "%"
+    asserted["mem_free"][1].extend( ["Physical memory free: " + str(mem_free) + "%"] )
 
     if data["swap_util"] <= 2:
         asserted["swap_util"][0] = 5
@@ -152,24 +109,25 @@ def assert_data(data):
         asserted["swap_util"][0] = 3
     else:
         asserted["swap_util"][0] = 1
-    asserted["swap_util"][1] = "SWAP Ultilization: " + str(data["swap_util"]) + "%"
+    asserted["swap_util"][1].extend( ["SWAP Ultilization: " + str(data["swap_util"]) + "%"] )
 
+    # print(asserted)
     return asserted
 
 
 def get_score(asserted):
     checklist = [
         ["STT", "Hạng mục kiểm tra", "Score"],
-        [1, "Kiểm tra trạng thái phần cứng", ["", ""]],
-        [2, "Kiểm tra nhiệt độ", ["", ""]],
-        [3, "Kiểm tra phiên bản ILOM", ["", ""]],
-        [4, "Kiểm tra phiên bản Image", ["", ""]],
-        [5, "Kiểm tra cấu hình RAID và dung lượng phân vùng OS", ["", ""]],
-        [6, "Kiểm tra cấu hình Bonding Network", ["", ""]],
-        [7, "Kiểm tra CPU Utilization", ["", ""]],
-        [8, "Kiểm tra CPU Load Average", ["", ""]],
-        [9, "Kiểm tra Memory", ["", ""]],
-        [10, "Kiểm tra Swap", ["", ""]],
+        [1, "Kiểm tra trạng thái phần cứng", [ "", [] ]],
+        [2, "Kiểm tra nhiệt độ", ["", [] ]],
+        [3, "Kiểm tra phiên bản ILOM", ["", [] ]],
+        [4, "Kiểm tra phiên bản Image", ["", [] ]],
+        [5, "Kiểm tra cấu hình RAID và dung lượng phân vùng OS", ["", [] ]],
+        [6, "Kiểm tra cấu hình Bonding Network", ["", [] ]],
+        [7, "Kiểm tra CPU Utilization", ["", [] ]],
+        [8, "Kiểm tra CPU Load Average", ["", [] ]],
+        [9, "Kiểm tra Memory", ["", [] ]],
+        [10, "Kiểm tra Swap", ["", [] ]],
     ]
 
     keys = list(asserted)
@@ -194,6 +152,7 @@ def drw_table(doc, checklist, row, col, info=False):
         return -1
     tab = doc.add_table(row, col)
     tab.alignment = WD_TABLE_ALIGNMENT.CENTER
+    # tab.style = WD_STYLE.TABLE_LIGHT_GRID
     tab.style = "Table Grid"
 
     # ADD TITLE CELLS AND COLOR THEM
@@ -214,7 +173,7 @@ def drw_table(doc, checklist, row, col, info=False):
             cells = rows.cells
             for j in range(0, len(checklist[i])):
                 cells[j].text = checklist[i][j]
-                cells[j].paragraphs[0].style = "Table Contents"
+                cells[j].paragraphs[0].style = "Table Paragraph"
     else:
         for i in range(1, len(checklist)):
             rows = tab.rows[i]
@@ -222,10 +181,10 @@ def drw_table(doc, checklist, row, col, info=False):
             for j in range(0, len(checklist[i])):
                 if j == (len(checklist[i]) - 1):
                     cells[j].text = checklist[i][j][0]
-                    cells[j].paragraphs[0].style = "Table Contents"
+                    cells[j].paragraphs[0].style = "Table Paragraph"
                     continue
                 cells[j].text = str(checklist[i][j])
-                cells[j].paragraphs[0].style = "Table Contents"
+                cells[j].paragraphs[0].style = "Table Paragraph"
     return tab
 
 
@@ -241,7 +200,8 @@ def drw_info(doc, node, checklist, images=[]):
                 os.path.normpath("output/" + node + "/" + images[i - 1]),
                 width=Inches(6.73),
             )
-        doc.add_paragraph(checklist[i][2][1])
+        for line in checklist[i][2][1]:
+            doc.add_paragraph(line, style='Dash List')
     doc.add_page_break()
 
 
@@ -312,6 +272,7 @@ def drw_doc(doc, input, force):
 def print_style(doc):
     styles = doc.styles
     # p_styles = [s for s in styles if s.type == WD_STYLE_TYPE.PARAGRAPH]
+    # p_styles = [s for s in styles if s.type == WD_STYLE_TYPE.TABLE]
     # for style in p_styles:
     for style in styles:
         print(style.name)
@@ -323,7 +284,8 @@ def run(input, output, verbose=False, force=False):
         doc = drw_doc(doc, input, force)
         if doc == -1:
             return -1
-    except:
+    except Exception as err:
+        print(err)
         return -1
 
     if verbose:
