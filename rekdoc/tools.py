65b2a330aab95030575e0cbb42154765346c76c3
rek3000 <torek3k@gmail.com>
2023-11-03 11:18:04 +0700

    test subprocess

--- rekdoc/tools.py
+++ rekdoc/tools.py
@@ -1,5 +1,5 @@
 import json
-import sys, re, io, os
+import sys, re, io, os, subprocess
 from wand.image import Image
 from wand.drawing import Drawing
 from wand.color import Color
@@ -93,6 +93,25 @@ def drw_text_image(text, file):
 
 
 ##### BASE ######
+def run(command):
+    process = subprocess.Popen(
+            command,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+    )
+    stdout_stream = process.stdout.read()
+    stderr_stream = process.stderr.read()
+    returncode = process.wait()
+
+    if not isinstance(stdout_stream, str):
+        stdout_stream = stdout_stream.decode('utf-8')
+    if not isinstance(stderr_stream, str):
+        stderr_stream = stderr_stream.decode('utf-8')
+    stdout = stdout_stream.splitlines()
+    stderr = stderr_stream.splitlines()
+
+    return stdout, stderr, returncode
+
 # TODO: REWRITE IN STRINGIO
 def cat(path, stdout=True, debug=False):
     try:
