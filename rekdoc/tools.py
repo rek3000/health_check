f5de7ae8110760f47c02a738c07b0eb6b7b7cf20
rek3000 <torek3k@gmail.com>
2023-11-08 16:45:08 +0700

    remove unused code

--- rekdoc/tools.py
+++ rekdoc/tools.py
@@ -70,28 +70,8 @@ def rm_ext(file, ext):
     return file[: -len(ext) - 1]
 
 
-##### IMAGEMAGICK #####
+##### IMAGE GENERATE METHOD #####
 # transform text to a png image file
-# def drw_text_image(text, file):
-#     with Image(width=1000, height=1000, background=Color("black")) as img:
-#         img.format = "png"
-#         with Drawing() as context:
-#             tmp = text.getvalue()
-#             metrics = context.get_font_metrics(img, tmp, True)
-#             x = 10
-#             y = 14
-#             w = int(metrics.text_width * 1.2)
-#             h = int(metrics.text_height * 1.3)
-#             img.resize(w, h)
-#             # context.font_family = "monospace"
-#             context.font_size = y
-#             context.fill_color = Color("white")
-#             context.gravity = "north_west"
-#             context.text_antialias = True
-#             context.text(x, y, text.getvalue())
-#             context(img)
-#         img.save(filename="PNG24:" + file)
-
 def drw_text_image(text, file):
     size = 14
     try:
@@ -114,7 +94,7 @@ def drw_text_image(text, file):
             y = y + attSpacing
         img_resize.save(file, format='PNG')
 
-##### END OF IMAGEMAGICK #####
+##### END OF IMAGE #####
 
 
 ##### BASE ######
@@ -124,8 +104,6 @@ def run(command, tokenize):
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    # stderr_stream = process.stderr.read()
-    # stdout_stream = process.stdout.read()
     stdout_stream, stderr_stream = process.communicate()
     returncode = process.wait()
 
@@ -163,74 +141,4 @@ def grep(path, regex, single_match, next=0, debug=False):
     stdout, stderr, code = run(command, False)
 
     return stdout
-
-
-# TODO: REWRITE IN STRINGIO
-# def cat(path, stdout=True, debug=False):
-#     try:
-#         with open(path, "r") as file:
-#             content = file.readlines()
-#             if stdout:
-#                 count = 0
-#                 result = ""
-#                 for l in content:
-#                     result += l.lstrip()
-#                     count += 1
-#                     if debug:
-#                         print(count + 1, ": ", l, sep="", end="")
-#                 return result
-#             else:
-#                 result = io.StringIO()
-#                 count = 0
-#                 for l in content:
-#                     result.write(l)
-#                     count += 1
-#                     if debug:
-#                         print(count + 1, ": ", l, sep="", end="")
-#                 return result
-#     except Exception as err:
-#         raise RuntimeError("Cannot open file to read") from err
-#         return -1
-
-
-# return the matched line along with next `n` lines
-# not so optimized but usable
-# def cursed_grep(path, regex, number=0, debug=False):
-#     result = io.StringIO()
-#     # call grep() then get line number returned
-#     line_number = int(grep(path, regex, True, True, debug=debug).split()[0][:-1])
-#     with open(path, "r") as f:
-#         lines = f.readlines()[line_number - 1 :]
-#         for i in range(number):
-#             result.write(lines[i])
-#     return result
-#
-#
-# # TODO: remake using StringIO
-# def grep(path, regex, single_line=True, print_line=False, debug=False):
-#     result = ""
-#     flag = re.MULTILINE
-#     pattern = re.compile(regex, flag)
-#     file = cat(os.path.normpath(path), False)
-#     content = file.getvalue().split("\n")
-#
-#     if single_line:
-#         for line in range(len(content)):
-#             if re.search(pattern, content[line]):
-#                 result += content[line].lstrip()
-#                 if print_line:
-#                     result = str(line + 1) + ": " + result
-#                 break
-#     else:
-#         for line in range(len(content)):
-#             if re.search(pattern, content[line]):
-#                 result += content[line].lstrip()
-#                 if print_line:
-#                     result = str(line + 1) + ": " + result
-#     if debug:
-#         print(result)
-#         print()
-#     return result
-
-
 ##### END BASE #####
